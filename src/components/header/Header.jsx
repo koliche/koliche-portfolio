4d476990fb1b39c35084bdb7c5c4092526a86e3d
M.koliche <66259634+koliche@users.noreply.github.com>
2022-05-27 13:07:55 +0100

    add the gheader section contant

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import "./header.css"
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="container header_container">
+      <h5>Hello I am </h5>
+      <h1>Mohamed Ait Koliche</h1>
+      <h5 className="text-light">Software Ingineer Student</h5>
+
+      <CTA />
+      <HeaderSocials />
+
+      <div className="me">
+        <img src={ME} alt="me" />
+      </div>
+
+      <a href="#contact" className='scroll__down'>Scroll Down</a>
+    </div>
   )
 }
 
